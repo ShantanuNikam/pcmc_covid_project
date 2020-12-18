b8edaadb0b5ab8c7707af483756e417589c9ad2d
ShantanuNikam <52479520+ShantanuNikam@users.noreply.github.com>
2020-12-18 09:51:17 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,124 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+import pandas as pd
+from dash.dependencies import Input, Output
+from graph import pcmc_graph, pie_age_chart, pie_gender_chart, pcmc, pcmc_pos_rec
+from table import table, table2, symptoms, stateList
+
+
+# external css
+external_stylesheets = [dbc.themes.BOOTSTRAP]
+
+# app
+app = dash.Dash(
+    __name__, external_stylesheets=external_stylesheets,)
+# app layout
+app.layout = html.Div([
+    html.Div(html.H1("PCMC COVID-19 Tracker"), className="app_heading"),
+    html.Div([
+        html.Div([
+            html.P(["last update on : ", html.Span("20 sept")],
+                   style={'color': "#ffffff"}),
+            dbc.Row([
+                dbc.Col(html.Div([html.H3(70140), html.P("Total Positive Cases")],
+                                 className="main_div", style={"backgroundColor": "#8B0000"})),
+                dbc.Col(html.Div([html.H3(421), html.P("Today's Positive")],
+                                 className="main_div", style={"backgroundColor": "#00CED1"})),
+                dbc.Col(html.Div([html.H3(963), html.P("Today's Discharged")],
+                                 className="main_div", style={"backgroundColor": "#0000FF"})),
+            ]),
+            dbc.Row([
+                dbc.Col(html.Div([html.H3(7520), html.P("Active Cases")],
+                                 className="main_div", style={"backgroundColor": "	#FFA500"})),
+                dbc.Col(html.Div([html.H3(61848), html.P("Recovered")],
+                                 className="main_div", style={"backgroundColor": "	#008000"})),
+                dbc.Col(html.Div([html.H3(1168), html.P("Death")],
+                                 className="main_div", style={"backgroundColor": "rgb(128,0,128)"})),
+            ]),
+        ], className="app_pcmc_info"),
+        # pie chart
+
+        dbc.Row([
+            dbc.Col(dbc.Card([
+                dbc.CardBody([
+                    html.H4("COVID-19 Age Group Chart",
+                            className="card_title"),
+                    dcc.Graph(id="pie_age_chart", figure=pie_age_chart),
+                ])
+            ], className="card"), md=8, sm=12),
+            dbc.Col([dbc.Card([
+                table
+
+            ], className="card"),
+                dbc.Card([
+                    table2
+
+                ], className="card")]),
+
+        ]),
+        dbc.Row([
+            dbc.Col(
+                dbc.Card([
+                    dbc.CardBody([
+                        html.H4("COVID-19 Gender Wise Cases",
+                                className="card_title"),
+                        dcc.Graph(id="pie_gender_chart2",
+                                  figure=pie_gender_chart),
+                    ])
+                ], className="card"), md=8, sm=12
+            ),
+            dbc.Col(dbc.Card([
+                symptoms
+
+            ], className="card"),
+            ),
+
+        ]),
+
+        # table
+        # dbc.Col([dbc.Card([
+        #     dbc.Table(table_header + table_body, bordered=False)
+
+        # ], className="card"),
+        #     dbc.Card([
+        #         dbc.Table(table2_header + table2_body, bordered=False)
+
+        #     ], className="card")]),
+        # non pcmc  resident *****
+
+        dbc.Row([
+            dbc.Col(dbc.Card([
+                dbc.CardBody([
+                    html.H4("COVID-19 PCMC Day Wise Positive Cases",
+                            className="card_title"),
+                    dcc.Graph(id="pcmc_daywise_graph", figure=pcmc),
+                ])
+            ], className="card pcmc_graph"), md=6, sm=12),
+            dbc.Col(
+                dbc.Card([
+                    dbc.CardBody([
+                        html.H4("COVID-19 PCMC pre Day Cases",
+                                className="card_title"),
+                        dcc.Graph(id="pcmc_daywise_pos_rec_graph",
+                                  figure=pcmc_pos_rec),
+                    ])
+                ], className="card pcmc_graph"), md=6, sm=12
+            )
+        ]),
+
+        # edit
+        dbc.Card([
+            stateList
+
+        ], className="card"),
+
+    ], className="app_body")
+
+
+])
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
---
+++ graph.py
@@ -0,0 +1,146 @@
+
+import plotly.graph_objects as go
+import plotly.io as plt_io
+import pandas as pd
+import mysql.connector
+import numpy as np
+# themeing
+
+
+# dataframe
+#pcmc_data = pd.read_csv('pcmc-count-update-2.csv')
+#pie_age_data = pd.read_csv('age_count_2.csv')
+#pie_gender_data = pd.read_csv("gender_2.csv")
+
+#Database Connection
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="Password@10",
+  database="covidproject"
+)
+#Database dataextraction(pie_gender_data)
+mycursor = mydb.cursor()
+mycursor.execute("SELECT * FROM gender_2")
+temp=  mycursor.fetchall()
+pie_gender_data = pd.DataFrame(temp, columns =['','cat', 'count']) 
+
+#Database Dataextraction(pie_age_data)
+mycursor.execute("SELECT * FROM age_count_2")
+temp2=  mycursor.fetchall()
+pie_age_data= pd.DataFrame(temp2, columns =['FIELD1','cat','age', 'count']) 
+#Database Dataextraction(pcmc_data)
+mycursor.execute("SELECT * FROM pcmc_count_update_2")
+temp2=  mycursor.fetchall()
+pcmc_data= pd.DataFrame(temp2, columns =['FIELD1','date','DAY_WISE_POSITIVE_COVID_CASES','DAY_WISE_PROGRESSIVE_POSITIVE_COVID_CASES','DAY_WISE_RECOVERED_COVID_CASES']) 
+
+# pcmc day wise graph
+
+pcmc_graph = go.Figure()
+pcmc_graph.add_trace(go.Scatter(x=pcmc_data["date"], y=pcmc_data["DAY_WISE_PROGRESSIVE_POSITIVE_COVID_CASES"], fill='tozeroy', mode="lines+markers", name='DAY WISE PROGRESSIVE POSITIVE COVID CASES',
+                                marker=dict(
+    color="#B5B5B5",
+    opacity=0.5,)))
+pcmc_graph.add_trace(go.Scatter(x=pcmc_data["date"], y=pcmc_data["DAY_WISE_POSITIVE_COVID_CASES"], fill='tozeroy', mode="lines+markers", name="DAY WISE POSITIVE COVID CASES",
+                                marker=dict(
+    color="#31A889",
+    opacity=0.5,)))  # fill down to xaxis
+
+pcmc_graph.add_trace(go.Scatter(x=pcmc_data["date"], y=pcmc_data["DAY_WISE_RECOVERED_COVID_CASES"], fill='tozeroy', mode="lines+markers", name="DAY WISE RECOVERED COVID CASES",
+                                marker=dict(
+    color="#ED708B",
+    opacity=0.5,)))
+pcmc_graph.update_traces(marker=dict(size=6,
+                                     line=dict(width=1)))
+pcmc_graph.update_layout(hovermode="x unified",
+                         hoverlabel=dict(bgcolor="#636362",
+                                         font_color="white"),
+                         legend=dict(orientation="h", yanchor="bottom",
+                                     y=1.02, xanchor="right", x=1),
+                         template="simple_white")
+# plt_io..update({'paper_bgcolor': 'rgba(0,0,0,0)',
+#                                            'plot_bgcolor': 'rgba(0,0,0,0)'})
+
+# ["plotly", "plotly_white", "plotly_dark", "ggplot2", "seaborn", "simple_white", "none"]
+
+# pcmc_graph.layout.plot_bgcolor = '#262529'
+# pcmc_graph.layout.paper_bgcolor = '#262529'
+
+# pcmc_graph.show(config={"hovermode ": "compare"})
+
+
+# change graph background color
+# pcmc_graph
+pcmc_graph.layout.plot_bgcolor = '#fff'
+
+# pie chart
+
+
+pie_age_chart = go.Figure(
+    data=[go.Pie(values=pie_age_data['count'], labels=pie_age_data["cat"])])
+pie_age_chart.update_traces(hoverinfo='label+value', textinfo='value', textfont_size=20,
+                            marker=dict(line=dict(color='#ffffff', width=2)))
+pie_age_chart.update_layout(hovermode="x unified",
+                            hoverlabel=dict(bgcolor="#636362",
+                                            font_color="white", font_family="Arial"),
+                            legend=dict(orientation="h", yanchor="bottom",
+                                        y=1.02, xanchor="right", x=1),
+                            template="simple_white")
+
+
+pie_gender_chart = go.Figure(
+    data=[go.Pie(labels=pie_gender_data['cat'], values=pie_gender_data["count"], hole=.5)])
+pie_gender_chart.update_traces(hoverinfo='label+value', textinfo='value', textfont_size=20,
+                               marker=dict(line=dict(color='#ffffff', width=2)))
+pie_gender_chart.update_layout(hovermode="x unified",
+                               #                         hoverlabel=dict(bgcolor="#636362",
+                               #                                         font_color="white", font_family="Arial"),
+                               legend=dict(orientation="h", yanchor="bottom",
+                                           y=1.02, xanchor="right", x=1),
+                               template="simple_white")
+
+
+# --------------------------------------- dummy ----------------------
+pcmc = go.Figure()
+pcmc.add_trace(go.Scatter(x=pcmc_data["date"], y=pcmc_data["DAY_WISE_PROGRESSIVE_POSITIVE_COVID_CASES"], fill='tozeroy', mode="lines+markers", name='DAY WISE PROGRESSIVE POSITIVE COVID CASES',
+                          marker=dict(
+    color="#B5B5B5",
+    opacity=0.5,)))
+# pcmc.add_trace(go.Scatter(x=pcmc_data["date"], y=pcmc_data["DAY WISE POSITIVE COVID CASES"], fill='tozeroy', mode="lines+markers", name="DAY WISE POSITIVE COVID CASES",
+#                                 marker=dict(
+#     color="#31A889",
+#     opacity=0.5,)))  # fill down to xaxis
+
+# pcmc.add_trace(go.Scatter(x=pcmc_data["date"], y=pcmc_data["DAY WISE RECOVERED COVID CASES"], fill='tozeroy', mode="lines+markers", name="DAY WISE RECOVERED COVID CASES",
+#                                 marker=dict(
+#     color="#ED708B",
+#     opacity=0.5,)))
+pcmc.update_traces(marker=dict(size=0,
+                               line=dict(width=0)))
+pcmc.update_layout(hovermode="x unified",
+                   hoverlabel=dict(bgcolor="#636362",
+                                   font_color="white"),
+                   legend=dict(orientation="h", yanchor="bottom",
+                               y=1.02, xanchor="right", x=1),
+                   template="simple_white")
+
+
+pcmc_pos_rec = go.Figure()
+pcmc_pos_rec.add_trace(go.Scatter(x=pcmc_data["date"], y=pcmc_data["DAY_WISE_POSITIVE_COVID_CASES"], fill='tozeroy', mode="lines+markers", name="DAY WISE POSITIVE COVID CASES",
+                                  marker=dict(
+    color="#d7385e",
+    opacity=0.5,)))  # fill down to xaxis
+
+pcmc_pos_rec.add_trace(go.Scatter(x=pcmc_data["date"], y=pcmc_data["DAY_WISE_RECOVERED_COVID_CASES"], fill='tozeroy', mode="lines+markers", name="DAY WISE RECOVERED COVID CASES",
+                                  marker=dict(
+    color="#f6830f",
+    opacity=0.5,)))
+pcmc_pos_rec.update_traces(marker=dict(size=0,
+                                       line=dict(width=0)))
+
+pcmc_pos_rec.update_layout(hovermode="x unified",
+                           hoverlabel=dict(bgcolor="#636362",
+                                           font_color="white"),
+                           legend=dict(orientation="h", yanchor="bottom",
+                                       y=1.02, xanchor="right", x=1),
+                           template="simple_white")
